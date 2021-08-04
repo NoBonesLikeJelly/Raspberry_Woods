c84bca18ba7a8c878fc12286967b4da3de0eecbb
NoBonesLikeJelly <noboneslikejelly@gmail.com>
2021-08-04 21:32:24 +1200

    Updates

--- node_js/server.js
+++ node_js/server.js
@@ -1,23 +1,19 @@
 const http = require('http');
+const url = require('url');
+const stringDecoder = require("string_decoder").StringDecoder;
+const util = require ("util");
+const formidable = require("formidable");
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  
-    let body = '';
+const server = http.createServer(function(req, res){
 
-    if(req.method === 'POST' && req.url === '/test')
-	{
+    if(req.METHOD == 'GET'){
 
-		console.log(req.method);
-        console.log('RECEIVED SOMETHING!');
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Sent Response');
+        console.log('Wow that worked?!?');
 
-	}
-
+    }
     
 });
 
